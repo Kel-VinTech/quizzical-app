46311993bdbfcc40ec2344f0a81d607a6ff5728c
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-23 18:04:15 -0500

    made changes to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import firstSlide from './components/firstSlide'
+import MainPage from './components/MainPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="container">
-     <firstSlide />
+      
+      <MainPage />
     </div>
   )
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const mainPage = () => {
+    return (
+        <div>
+            <h2>Quizzical </h2>
+             <p>Some description if needed</p>
+            <button>Start quiz</button>
+        </div>
+    )
+}
+
+export default mainPage
