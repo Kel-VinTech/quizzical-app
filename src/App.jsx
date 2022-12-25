33fb518b6ed1ca255d38ecfba18856f0df4e3931
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-25 02:41:38 -0500

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MainPage from './components/MainPage'
 import Questionpage from './components/QuestionPage'
 
 function App() {
+  const [start, setStart] = useState(false)
   const [count, setCount] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [checked, setChecked] = useState(false)
+  const [question, setQuestion] = useState([])
 
+  const array = (arr) => arr.sort(() => Math.random() - 0.5)
+ useEffect(() => {
+ })
   return (
     <div className="container">
       
       {/* <MainPage /> */}
-      <Questionpage />
+      <Questionpage />                                         
     </div>
   )
 }
